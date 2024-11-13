1eda8b7c5e264b7ec6a7abf2312cfe0a7c30c4b3
mr10us <minenkoduck@gmail.com>
2024-11-13 16:38:08 +0200

    fix: fixed footer overflow
    – Because of dropdown initial positioning it was overflowing and caused page horizotal shift

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,25 +1,24 @@
 window.addEventListener("load", function () {
-  (function () {
+  const dropdowns = document.querySelectorAll(".dropdown");
+  dropdowns.forEach((dropdown) => {
+    if (dropdown.dataset.position) {
+      const position = dropdown.dataset.position;
+      const content = dropdown.querySelector(".dropdown__content");
+
+      if (position === "top") {
+        dropdown.style.setProperty("--triangle-deg", "-180deg");
+        dropdown.style.setProperty("--triangle-origin", "bottom center");
+        content.style.top = "unset";
+        content.style.bottom = "calc(100% + 8px)";
+      }
+    }
+  })(function () {
     window.addEventListener("click", ({ target }) => {
       if (
         target.classList.contains("dropdown") ||
         target.parentNode.classList.contains("dropdown")
       ) {
         const dropdown = target.closest(".dropdown");
-
-        // check if dropdown already has content
-        if (dropdown.hasChildNodes("div.dropdown__content")) {
-          const content = dropdown.querySelector(".dropdown__content");
-
-          const position = dropdown.dataset.position;
-
-          if (position === "top") {
-            dropdown.style.setProperty("--triangle-deg", "-180deg");
-            dropdown.style.setProperty("--triangle-origin", "bottom center");
-            content.style.top = "unset";
-            content.style.bottom = "calc(100% + 8px)";
-          }
-        }
 
         dropdown.classList.toggle("active");
       } else {
