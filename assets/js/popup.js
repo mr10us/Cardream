72f3d31b439120c63a76c6c16374fd5c5e1948ba
mr10us <minenkoduck@gmail.com>
2024-11-19 18:15:29 +0200

    fix: popup scroll disabled while active

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,18 +1,23 @@
 function openPopup(popupName) {
   document.querySelectorAll(".popup").forEach((popup) => {
     popup.classList.remove("active");
+    document.body.style.overflowY = "auto";
   });
 
   const popup = document.querySelector(`.popup.${popupName}`);
   const closeButton = popup.querySelector(".popup__close");
 
   popup.classList.add("active");
+  document.body.style.overflowY = "hidden";
+
   popup.addEventListener("click", ({ target }) => {
     if (target === popup) {
       popup.classList.remove("active");
+      document.body.style.overflowY = "auto";
     }
     if (target === closeButton || target.parentNode === closeButton) {
       popup.classList.remove("active");
+      document.body.style.overflowY = "auto";
     }
   });
 }
@@ -21,6 +26,9 @@ window.addEventListener("keydown", function ({ key }) {
   if (key === "Escape") {
     const popups = Array.from(document.querySelectorAll(".popup.active"));
 
-    popups.forEach((popup) => popup.querySelector(".popup__close").click());
+    popups.forEach((popup) => {
+      popup.querySelector(".popup__close").click();
+      document.body.style.overflowY = "auto"
+    });
   }
 });
